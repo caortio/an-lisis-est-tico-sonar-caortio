2589a7850d8a268a92cba027d707940d9afc7b6f
caortio <113450704+caortio@users.noreply.github.com>
2026-03-03 16:47:14 +0100

    Arreglo cuarta issue
    Updated constant name to follow Java naming conventions.

--- hipster-core/src/main/java/es/usc/citius/hipster/algorithm/NegativeCycleException.java
+++ hipster-core/src/main/java/es/usc/citius/hipster/algorithm/NegativeCycleException.java
@@ -2,7 +2,8 @@ package es.usc.citius.hipster.algorithm;
 
 
 public class NegativeCycleException extends RuntimeException {
-    private static final String message = "Existence of a negative cycle detected";
+    //Cuarta issue: cambiar el nombre de la constante para usar la convención de nomenclatura de constantes en Java.
+    private static final String MESSAGE = "Existence of a negative cycle detected";
 
     public NegativeCycleException() {
         super(message);
